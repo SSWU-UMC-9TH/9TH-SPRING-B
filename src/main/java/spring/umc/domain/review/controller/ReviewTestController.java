7fe49cc22f1b9a0f1506a079893229598a0f3114
ownue <enu_i@naver.com>
2025-11-14 00:06:31 +0900

    feat: api 응답 구조 통일, 에러 핸들러 기능 구현

--- src/main/java/spring/umc/domain/review/controller/ReviewTestController.java
+++ src/main/java/spring/umc/domain/review/controller/ReviewTestController.java
@@ -6,6 +6,8 @@ import spring.umc.domain.common.dto.PageResponseDto;
 import spring.umc.domain.review.dto.MyReviewResponseDto;
 import spring.umc.domain.review.service.ReviewService;
 import org.springframework.data.domain.Page;
+import spring.umc.global.apiPayload.ApiResponse;
+import spring.umc.global.apiPayload.code.GeneralSuccessCode;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class ReviewTestController {
      *  [GET] /api/test/reviews?userId=1&storeId=10
      */
     @GetMapping("/reviews")
-    public PageResponseDto<MyReviewResponseDto> getMyReviews(
+    public ApiResponse<PageResponseDto<MyReviewResponseDto>> getMyReviews(
             @RequestParam Long userId,
             @RequestParam(required = false) Long storeId,
             @RequestParam(required = false) String storeName,
@@ -29,6 +31,6 @@ public class ReviewTestController {
     ) {
         Page<MyReviewResponseDto> dto = reviewService.getMyReviews(userId, storeId, storeName, star, page, size);
 
-        return PageResponseDto.from(dto);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, PageResponseDto.from(dto));
     }
 }
