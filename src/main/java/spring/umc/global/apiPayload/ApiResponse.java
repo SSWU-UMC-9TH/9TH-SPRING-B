e15a572174b6c734740cebaa11d44224ec738a2a
ownue <enu_i@naver.com>
2025-11-19 18:23:40 +0900

    feat: week8 과제 구현

--- src/main/java/spring/umc/global/apiPayload/ApiResponse.java
+++ src/main/java/spring/umc/global/apiPayload/ApiResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 import spring.umc.global.apiPayload.code.BaseErrorCode;
 import spring.umc.global.apiPayload.code.BaseSuccessCode;
 
